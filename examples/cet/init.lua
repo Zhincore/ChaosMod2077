1ee34d46ed9dd48ed004cb9b432242f657f7fe30
Zhincore <adam@zhincore.eu>
2025-03-18 20:34:15 +0100

    e

--- examples/cet/init.lua
+++ examples/cet/init.lua
@@ -1,8 +1,13 @@
+-- This is an example of a Chaos Mod addon in CET Lua
+
 registerForEvent("onInit", function()
+    ---Import the ChaosMod so you can interact with it
     ---@type ChaosMod
     local ChaosMod = GetMod("ChaosMod2077")
 
+    -- Register your effect, you can register multiple by calling ChaosMod.RegisterEffect multiple times
     ChaosMod.RegisterEffect({
+        -- Unique id of your effect, remember to add an onscreen translation (in this case the key would be `ChaosMod-Effects-MyLua`)
         id = "MyLua",
         duration = ChaosMod.ChaosTimedType.Short,
         onStart = function()
--- scripts/cet/effect.lua
+++ scripts/cet/effect.lua
@@ -6,9 +6,9 @@ ChaosTimedType = {
 }
 
 ---@class Effect
----@field id string The unique identifier of the effect
----@field duration? ChaosTimedType The duration type of the effect
----@field incompatibleEffects? string[] List of ids of incompatible effects
+---@field id string The unique identifier of the effect.
+---@field duration? ChaosTimedType The duration type of the effect. Default is instant.
+---@field incompatibleEffects? string[] List of ids of incompatible effects.
 ---@field onStart function Called when the effect begins. Required.
 ---@field onUpdate? function Called every timer update while the effect is active. Optional.
 ---@field onStop? function Called when the effect ends, use this for clean up. Optional.
